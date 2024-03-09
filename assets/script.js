96c5aa340dd951be205e6652b12bd52236603038
Basil Goodluck <basilgoodluck22@gmail.com>
2024-03-09 02:54:21 +0100

    Updated

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,30 @@
-let hometext = document.querySelector('.homeBox h1');
-let para = document.getElementById("para")
+const textElement = document.getElementById('para');
+const messages = ["Where there is security", "We prioritize customers' privacy", "Don't hesitate to create an account now", "We will be glad to know you"]; // Array containing the different messages
+let messageIndex = 0; // Current index of the message being displayed
 
+function animateWriting(message) {
+    let index = 0;
 
+    function writeNextCharacter() {
+        if (index <= message.length) {
+            textElement.textContent = message.substring(0, index);
+            index++;
+            setTimeout(writeNextCharacter, 150); // Adjust the speed of animation (milliseconds)
+        } else {
+            // Animation for one message completes, move to the next message
+            messageIndex++;
+            if (messageIndex >= messages.length) {
+                // Reset the index to start from the beginning
+                messageIndex = 0;
+            }
+            // Call animateWriting() with the next message
+            animateWriting(messages[messageIndex]);
+        }
+    }
 
-
-const chars = hometext.textContent.split("")
-
-
-const charSpan = chars.map(e=>{
-    
-    span = document.createElement("span")
-    
-    span.textContent = e
-    return span
-    
-})
-
-hometext.innerHTML = ""
-
-charSpan.forEach(span => {
-    hometext.appendChild(span)  
-});
-
-const text = hometext.textContent
-let idx = 0
-
-// function animeWriting(){
-//     hometext.textContent = text.substring(0, idx)
-//     idx++
-//     if (idx > text.length){
-//         idx = 0
-        
-//     }setTimeout(animeWriting, 300)
-// }
-
-// animeWriting()
-
-let paraARR = ['We are the best', 'You should try us out', 'Don\'t miss out on this one', 'The only one file box']
-let paraTXT;
-
-
-function animationWRT (){
-    paraTXT = paraARR[idx]
-    para.textContent = paraTXT.substring(0, idx)
-    idx++
-    if(idx > paraARR[idx].length){
-        idx = 0
-    }setTimeout(animationWRT, 300)
-
-
+    // Start the writing animation for the current message
+    writeNextCharacter();
 }
-animationWRT()
+
+// Start the animation cycle with the first message
+animateWriting(messages[messageIndex]);
